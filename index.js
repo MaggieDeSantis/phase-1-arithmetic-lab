7b86e71b0c2a981e7cef3df258786fe6b70558f4
MaggieDeSantis <sheisntreal@gmail.com>
2023-03-17 15:38:46 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 2;
+let num2 = 31;
+let num3 = 20;
+let num4 = 8;
 
-// Write your code here
+let multiply = num1*num2;
+
+let mod = num3 % num4; 
+
+let max = Math.max(1,5,10,16,20);
+
+let random = Math.floor(Math.random()*10) + 1; 
+Math.isInteger(random);
